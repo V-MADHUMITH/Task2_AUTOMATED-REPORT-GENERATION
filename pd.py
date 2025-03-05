bfc438fcb85a4677797537145ee552af6127884e
V.MADHUMITHA <119583333+V-MADHUMITH@users.noreply.github.com>
2025-03-05 21:52:50 +0530

    pd.py

--- pd.py
+++ pd.py
@@ -1,17 +1,9 @@
-
-
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-plt.figure(figsize=(8,5))
 import pandas as pd
-
-
+plt.figure(figsize=(8,5))
 df = pd.read_csv(r"C:\Users\madhu\Documents\task 2\data.csv")
-
-
 print(df.head())  
-
 sns.histplot(df["Salary"], bins=5, kde=True, color="red")  
 plt.title("Salary Distribution")
 plt.xlabel("Salary")
